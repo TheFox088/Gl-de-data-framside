12f88dbe2400b3f688b20243da3a9bc0f7e30dee
Sandra.B <105020067+Sandra@users.noreply.github.com>
2023-12-06 08:54:58 +0100

    gløde butikken

--- Dell Latitude engelsk/script.js
+++ Dell Latitude engelsk/script.js
@@ -23,3 +23,34 @@ document.addEventListener("DOMContentLoaded", function() {
         form.reset();
     });
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+    var quantityInput = document.getElementById('quantity');
+    var form = document.getElementById('cartForm');
+
+    // Add event listener for changes in quantity
+    quantityInput.addEventListener('input', function () {
+        // You can perform actions here, e.g., update total price
+        updateTotalPrice();
+    });
+
+    // Function to update total price (replace this with your logic)
+    function updateTotalPrice() {
+        // Example: Get quantity value and calculate total price
+        var quantity = parseInt(quantityInput.value);
+        var unitPrice = 10; // Replace with your actual unit price
+        var totalPrice = quantity * unitPrice;
+
+        // Display or use the total price as needed
+        console.log('Total Price: $' + totalPrice);
+    }
+
+    // Add other event listeners or functions as needed
+
+    // Prevent the default form submission for demonstration purposes
+    form.addEventListener('submit', function (event) {
+        event.preventDefault();
+        // Perform your form processing logic here
+        console.log('Form submitted!');
+    });
+});
